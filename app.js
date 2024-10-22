e9a4a4d274e19172dadf03649670ba53ea3be009
Charlotte Lund <chalund@gmail.com>
2024-10-22 15:39:31 +0200

    attribute binding, dynamic classes, toggling favs, computed properties

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: "https://www.google.com",
       showBooks: true,
-      title: "The final Empire",
-      author: "Brandon Sanderson",
-      age: 45,
-      x: 0,
-      y: 0
+      books: [
+        { title: "name of the Wind", author: "Patrick Rothfuss", img: "assets/1.jpg", isFav: true },
+        { title: "The Wise Man's Fear", author: "Patrick Rothfuss", img: "assets/2.jpg", isFav: false    },
+        { title: "The final Empire", author: "Brandon Sanderson", img: "assets/3.jpg", isFav: true  },
+      ],
     };
   },
   methods: {
-    handleEvent(e) {
-      console.log(e, e.type);
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
     },
 
-    handleEvent(e, data) {
-      console.log(e, e.type)
-      if(data) {
-        console.log(data)
-      }
-    },
-
-    handleMousemove(e) {
-        this.x = e.offsetX;
-        this.y = e.offsetY;
+    toggleFav(book) {
+      book.isFav = !book.isFav;
     }
   },
+  computed: {
+    filteredBooks() {
+        return this.books.filter((book) => book.isFav);
+    }
+  }
 });
 
 app.mount("#app");
